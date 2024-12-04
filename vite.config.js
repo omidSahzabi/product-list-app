f94136cea6bc00a08e96169d7df884f8cfbc5e09
omid <omidpanahiyan5700@gmail.com>
2024-12-04 13:13:13 +0330

    trying to depoy on gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/product-list-app',
+  plugins: [react(), Pages()],
 })
